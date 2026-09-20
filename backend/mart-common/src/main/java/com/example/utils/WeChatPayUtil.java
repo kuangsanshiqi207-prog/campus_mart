0772774410a5888c9773ebb8cc46c1f4ee3463e6
kuangsanshiqi207-prog <kuangsanshiqi207@gmail.com>
2026-09-20 18:10:12 +0800

    feat(backend): complete backend module with Spring Boot 3.4.6

--- backend/mart-common/src/main/java/com/example/utils/WeChatPayUtil.java
+++ backend/mart-common/src/main/java/com/example/utils/WeChatPayUtil.java
@@ -2,7 +2,7 @@ package com.example.utils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.sky.properties.WeChatProperties;
+import com.example.properties.WeChatProperties;
 import com.wechat.pay.contrib.apache.httpclient.WechatPayHttpClientBuilder;
 import com.wechat.pay.contrib.apache.httpclient.util.PemUtil;
 import org.apache.commons.lang.RandomStringUtils;
